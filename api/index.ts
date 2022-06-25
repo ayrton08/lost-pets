ddaf01d4bcfce5a148e51cc1e53f08e96c79e455
ayrton08 <ayrtonjuarez90@gmail.com>
2022-06-25 20:31:14 -0300

    fix(api): port listening

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import * as path from "path";
 import { routerApi } from "./routes";
 import * as cors from "cors";
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 const staticDir = path.resolve(__dirname, "../dist");
