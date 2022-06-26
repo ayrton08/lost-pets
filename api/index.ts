599b3212bd1ad10c1088adeb0fcb01f75b9b3bd3
ayrton08 <ayrtonjuarez90@gmail.com>
2022-06-25 21:49:50 -0300

    fix(api): express.static

--- api/index.ts
+++ api/index.ts
@@ -22,3 +22,9 @@ app.get("/test", async (req, res) => {
 });
 
 routerApi(app);
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
